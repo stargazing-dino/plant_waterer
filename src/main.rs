66bc65690fd5f7f45eaf40bb10ec298f4dc59172
Rex Magana <Rex.magana@gmail.com>
2024-10-22 09:30:23 -0600

    Async watering on button press

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 #![no_std]
 #![no_main]
 
+use crate::embassy_nrf::gpio::{Level, Output, OutputDrive};
 use embassy_executor::Spawner;
-use embassy_time::Timer;
 use microbit_bsp::*;
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
+    let board = Microbit::new(Default::default());
+
+    // Use button A to control the pump
+    let mut button = board.btn_a;
+
+    // Use P0 as the output pin to control the MOSFET
+    let mut pump_control = Output::new(board.p1, Level::Low, OutputDrive::Standard);
+
     loop {
-        defmt::info!("Hello World!");
-        Timer::after_millis(1000).await;
+        button.wait_for_low().await;
+
+        defmt::info!("Watering plant...");
+        pump_control.set_high();
+
+        button.wait_for_high().await;
+
+        pump_control.set_low();
+        defmt::info!("Watering complete.");
     }
 }
