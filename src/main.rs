6e36378ba4bf4c2adffc2327d3fd8dc86522ab64
Rex Magana <Rex.magana@gmail.com>
2024-11-12 18:36:02 -0800

    simpler

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@
 
 use embassy_executor::Spawner;
 use embassy_time::Timer;
-use microbit_bsp::*;
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
+    let _ = embassy_nrf::init(Default::default());
+
     loop {
         defmt::info!("Hello World!");
         Timer::after_millis(1000).await;
